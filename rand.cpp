0e229b0108dd36a6d7043e7e794d111067962e0b
zchengyun <1690132420@qq.com>
2021-09-02 23:56:10 +0800

    添加项目文件。

---
+++ rand.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cstdlib>
+int main()
+{
+    srand(0); // 以当前时间为随机生成器的种子,0为38？1对应41？2对应45？
+    int rand_variable = rand();//返回0-RAND_MAX间的随机整数值，最大值0x7fff
+    std::cout << rand_variable << std::endl;
+    std::cout << RAND_MAX << std::endl;
+
+    srand(time(NULL));
+    std::cout << time(NULL) << std::endl;// 返回从1970年元旦午夜0点到现在的秒数。
+    std::cout << rand() << std::endl;
+
+}
+
